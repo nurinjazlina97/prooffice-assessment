b66dcf2a61c46200d0c697220c5c41863effa44f
nurinjazlina97 <nurynn.nj@gmail.com>
2022-12-15 13:18:45 +0800

    Add button to order page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,9 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+
+                    <a href="{{ route('orders') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Order Page</a>
                 </div>
             </div>
         </div>
